5aeea870fa35fb395794531b57a2620bd2753ed2
jessica-sobreira <j.jecristina@gmail.com>
2024-03-16 22:06:22 -0300

    persist

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,13 @@
 import { RouterProvider } from "react-router-dom";
-import { routes } from "./routes/routes";
+// import { routes } from "./routes/routes";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 
 
 export const Root = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
+        // <ThemeProvider theme={theme}>
+            // <CssBaseline />
             <RouterProvider router={routes} />
-        </ThemeProvider>
+        // </ThemeProvider>
     );
 };
