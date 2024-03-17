92ea14382f7bc453973bffa19499bdcb74a5a890
jessica-sobreira <j.jecristina@gmail.com>
2024-03-16 22:03:34 -0300

    iniciando

---
+++ src/Root.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes/routes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+
+export const Root = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <RouterProvider router={routes} />
+        </ThemeProvider>
+    );
+};
